0b45a4e4f53b74b02042c9e57c55510f90d3912f
Cameron Yee <yee.camero23@gmail.com>
2017-09-24 16:32:24 -0600

    misc changes

---
+++ parallel_project/js/scripts.js
@@ -0,0 +1,7 @@
+function initMap() {
+    var map;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 38.894338, lng: -104.802753 },
+        zoom: 10
+    }
+}
