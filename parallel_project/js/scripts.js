b90d7e193543e4bd90b4d75223a53b0f84d9e98e
Cameron Yee <yee.camero23@gmail.com>
2017-09-24 20:09:50 -0600

    Styling, file organization

--- parallel_project/js/scripts.js
+++ parallel_project/js/scripts.js
@@ -1,7 +1,28 @@
-function initMap() {
+var initMap = function() {
     var map;
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 38.894338, lng: -104.802753 },
         zoom: 10
+    })
+    var marker = new google.maps.Marker({
+        position: { lat: 38.894338, lng: -104.802753 },
+        map: map
+    });
+}
+
+var activeBtn = function () {
+    $(document).removeClass('active');
+    var pname = location.pathname.toString;
+    if (pname.includes('index')) {
+        $('#homebtn').addClass('active');
+    } else if (pname.includes('products')) {
+        $('#productbtn').addClass('active');
+    } else if (pname.includes('about')) {
+        $('#aboutbtn').addClass('active');
     }
 }
+
+$(document).ready(function () {
+    initMap();
+    activeBtn();
+});
