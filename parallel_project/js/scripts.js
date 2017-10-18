117cd518375a24833301ce83ce326aa5567be2cf
Cameron Yee <yee.camero23@gmail.com>
2017-10-18 10:25:31 -0600

    Added rotating picutre images and script, function loads image based on time, but it doesn't rotate unless page is reloaded

--- parallel_project/js/scripts.js
+++ parallel_project/js/scripts.js
@@ -17,7 +17,45 @@ var activeBtn = function () {
     $('.top-menu a[href="'+pathname+'"]').addClass('active');
 }
 
+var rotatePictures = function () {
+    var prefix = "images/home-bike-";
+    var images = [];
+    var counter = new Date().getSeconds();
+
+    for(var i = 1; i < 7; i++) {
+        images.push(prefix + i + ".jpg");
+    }
+
+    switch(true) {
+        case counter < 11: 
+            document.getElementById('img-placeholder').setAttribute('src', images[0]);
+            console.log("case 1");
+            break;
+        case counter < 21: 
+            document.getElementById('img-placeholder').setAttribute('src', images[1]);
+            console.log("case 2");
+            break;
+        case counter < 31: 
+            document.getElementById('img-placeholder').setAttribute('src', images[2]);
+            console.log("case 3");
+            break;
+        case counter < 41: 
+            document.getElementById('img-placeholder').setAttribute('src', images[3]);
+            console.log("case 4");
+            break;
+        case counter < 51: 
+            document.getElementById('img-placeholder').setAttribute('src', images[4]);
+            console.log("case 5");
+            break;
+        case counter < 60: 
+            document.getElementById('img-placeholder').setAttribute('src', images[5]);
+            console.log("case 6");
+            break;
+    }
+}
+
 $(document).ready(function () {
+    rotatePictures();
     initMap();
     activeBtn();
 });
